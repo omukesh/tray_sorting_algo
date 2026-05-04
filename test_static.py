1dc3b9dc92467b447832a033df848fdb6b30151c
omukesh <letters2mukesh@gmail.com>
2026-05-04 19:21:55 +0530

    change in basic logic image

--- test_static.py
+++ test_static.py
@@ -12,7 +12,7 @@ from tray_analyzer import TrayAnalyzer
 # ── config ──────────────────────────────────────────────────
 MODEL_PATH  = "weights/best.pt"     
 TEST_IMAGES = [
-    "input/10.jpg",
+    "input/14.jpg",
      #"input/6.png",
     # "input/empty_tray.png",
 ]
